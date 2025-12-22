dd698207157bd8407bd4ab1e777e297a6dc8917f
hack-y-hirata <142282295+hack-y-hirata@users.noreply.github.com>
2025-12-22 19:54:21 +0900

    Add files via upload

---
+++ kadai_007/associative_array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_007
+
+    </title>
+</head>
+<body>
+    
+    <p>
+
+        <?php
+            $associative_array=["name"=>"onion","price"=>200,"weight"=>160];
+
+            print_r($associative_array);
+        ?>
+    </p>
+
+
+
+
+</body>
+</html>
